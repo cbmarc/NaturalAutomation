b305c390abd2f9a19b9f639e5521fefaec76a78d
Marc <marc@fullstackmarc.com>
2017-11-07 16:02:49 +0100

    Initial commit

---
+++ src/test/java/com/fullstackmarc/selenium/config/AcceptanceTestsConfiguration.java
@@ -0,0 +1,14 @@
+package com.fullstackmarc.selenium.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AcceptanceTestsConfiguration {
+
+//    @Bean(name = "sharedSteps")
+//    public SharedSteps sharedSteps() {
+//        return new SharedSteps();
+//    }
+}
