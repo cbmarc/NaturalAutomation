3dee9156a6910bfca89be9b2fd0a4e83e7dadb38
Marc <marc@fullstackmarc.com>
2017-11-09 10:17:11 +0100

    Update name of runner to adapt to maven conventions

--- src/test/java/com/fullstackmarc/selenium/JBehaveStoriesTest.java
+++ src/test/java/com/fullstackmarc/selenium/JBehaveStoriesTest.java
@@ -2,10 +2,16 @@ package com.fullstackmarc.selenium;
 
 import com.fullstackmarc.selenium.jbehave.AbstractSpringJBehaveStories;
 import com.fullstackmarc.selenium.jbehave.AcceptanceTest;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @AcceptanceTest
-public class JBehaveStoryRunner extends AbstractSpringJBehaveStories {
+public class JBehaveStoriesTest extends AbstractSpringJBehaveStories {
+
+    @Test
+    public void testIt() throws Exception {
+        System.out.println("BLAH");
+    }
 }
