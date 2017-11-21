7ba1de30e7b33bab3fcc4aa345a9bdead1c8d955
Marc <marc@fullstackmarc.com>
2017-11-21 13:06:33 +0100

    Refactor names

--- src/main/java/com/fullstackmarc/fwkpoc/selenium/pages/PageFactory.java
+++ src/main/java/com/fullstackmarc/fwkpoc/selenium/pages/PageFactory.java
@@ -3,21 +3,20 @@ package com.fullstackmarc.fwkpoc.selenium.pages;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
 import java.util.Set;
 
 @Component
 public class PageFactory {
 
-    private final Set<? extends Named> pageMap;
+    private final Set<? extends Named> pages;
 
     @Autowired
-    public <T extends Page & Named> PageFactory(Set<? extends T> pageMap) {
-        this.pageMap = pageMap;
+    public <T extends Page & Named> PageFactory(Set<? extends T> pages) {
+        this.pages = pages;
     }
 
     public Page getPage(String name) {
-        Named namedPage = pageMap.stream().filter(p -> p.getName().equals(name)).findFirst().get();
+        Named namedPage = pages.stream().filter(p -> p.getName().equals(name)).findFirst().get();
         return (Page) namedPage;
     }
 }
