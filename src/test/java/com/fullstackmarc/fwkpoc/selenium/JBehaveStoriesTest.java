d5178ae4c48b7c5c49ded89addfb9e99d3246e37
Marc <marc@fullstackmarc.com>
2017-11-22 17:58:41 +0100

    Add ability to add all named pages, specially to generify test

--- src/test/java/com/fullstackmarc/fwkpoc/selenium/JBehaveStoriesTest.java
+++ src/test/java/com/fullstackmarc/fwkpoc/selenium/JBehaveStoriesTest.java
@@ -2,7 +2,7 @@ package com.fullstackmarc.fwkpoc.selenium;
 
 import com.fullstackmarc.fwkpoc.jbehave.AbstractSpringJBehaveStories;
 import com.fullstackmarc.fwkpoc.jbehave.AcceptanceTest;
-import com.fullstackmarc.fwkpoc.selenium.pages.Page;
+import com.fullstackmarc.fwkpoc.selenium.pages.Named;
 import com.fullstackmarc.fwkpoc.selenium.pages.PageFactory;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
@@ -11,6 +11,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
 
 
 @RunWith(SpringRunner.class)
@@ -23,7 +25,8 @@ public class JBehaveStoriesTest extends AbstractSpringJBehaveStories {
 
     @Test
     public void pageMapDidLoad() throws Exception {
-        Page page = pageFactory.getPage("Google");
-        Assert.assertThat(page, Matchers.notNullValue());
+        Set<? extends Named> pages = pageFactory.getPages();
+        Assert.assertThat(pages, Matchers.notNullValue());
+        Assert.assertThat(pages.size(), Matchers.greaterThan(0));
     }
 }
