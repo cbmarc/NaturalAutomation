51d2a8717c325d0bf874b2d1c55948655d388c92
Marc <marc@fullstackmarc.com>
2017-11-27 10:13:53 +0100

    Organizational fixes Page abstract refactoring

--- src/test/java/com/fullstackmarc/fwkpoc/selenium/JBehaveStoriesTest.java
+++ src/test/java/com/fullstackmarc/fwkpoc/selenium/JBehaveStoriesTest.java
@@ -2,17 +2,11 @@ package com.fullstackmarc.fwkpoc.selenium;
 
 import com.fullstackmarc.fwkpoc.jbehave.AbstractSpringJBehaveStories;
 import com.fullstackmarc.fwkpoc.jbehave.AcceptanceTest;
-import com.fullstackmarc.fwkpoc.selenium.pages.Named;
 import com.fullstackmarc.fwkpoc.selenium.pages.PageFactory;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Set;
 
 
 @RunWith(SpringRunner.class)
@@ -20,13 +14,5 @@ import java.util.Set;
 @AcceptanceTest
 public class JBehaveStoriesTest extends AbstractSpringJBehaveStories {
 
-    @Autowired
-    private PageFactory pageFactory;
 
-    @Test
-    public void pageMapDidLoad() throws Exception {
-        Set<? extends Named> pages = pageFactory.getPages();
-        Assert.assertThat(pages, Matchers.notNullValue());
-        Assert.assertThat(pages.size(), Matchers.greaterThan(0));
-    }
 }
