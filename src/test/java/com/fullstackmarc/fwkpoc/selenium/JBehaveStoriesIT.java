1b8f30a714f9f04397e12febbc7d2e3f7b0c07ba
lucbel <roduranro@gmail.com>
2017-12-01 11:14:04 +0100

    move annotations to template and disable web application

--- src/test/java/com/fullstackmarc/fwkpoc/selenium/JBehaveStoriesIT.java
+++ src/test/java/com/fullstackmarc/fwkpoc/selenium/JBehaveStoriesIT.java
@@ -1,15 +1,9 @@
 package com.fullstackmarc.fwkpoc.selenium;
 
 import com.fullstackmarc.fwkpoc.jbehave.AbstractSpringJBehaveStories;
-import com.fullstackmarc.fwkpoc.jbehave.AcceptanceTest;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AcceptanceTest
+
 public class JBehaveStoriesIT extends AbstractSpringJBehaveStories {
 
 
