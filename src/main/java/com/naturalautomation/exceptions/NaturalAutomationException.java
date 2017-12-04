a63fd6da6ef1d891a56e65b1b16e72f774df3e5a
lucbel <roduranro@gmail.com>
2017-12-04 15:33:45 +0100

    new Exception and inheritance

---
+++ src/main/java/com/naturalautomation/exceptions/NaturalAutomationException.java
@@ -0,0 +1,23 @@
+package com.naturalautomation.exceptions;
+
+public class NaturalAutomationException extends RuntimeException {
+
+    public NaturalAutomationException() {
+    }
+
+    public NaturalAutomationException(String s) {
+        super(s);
+    }
+
+    public NaturalAutomationException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public NaturalAutomationException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public NaturalAutomationException(String s, Throwable throwable, boolean b, boolean b1) {
+        super(s, throwable, b, b1);
+    }
+}
--- src/main/java/com/naturalautomation/exceptions/NotInPageException.java
+++ src/main/java/com/naturalautomation/exceptions/NotInPageException.java
@@ -2,7 +2,7 @@ package com.naturalautomation.exceptions;
 
 import com.naturalautomation.selenium.pages.Page;
 
-public class NotInPageException extends Exception {
+public class NotInPageException extends NaturalAutomationException {
     private Page page;
 
     public NotInPageException(Page page) {
